3faa24464ce66ce11792e4a2e387805105a2308b
Anthony Siletti <asiletti@icloud.com>
2022-12-01 16:54:24 -0500

    show page

---
+++ models/ticket.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/,
+  },
+  price: {
+    type: Number,
+    min: 0,
+  },
+  flight: {
+    type: Schema.Types.ObjectId,
+    ref: 'Flight',
+  },
+});
+
+module.exports = model('Ticket', ticketSchema);
